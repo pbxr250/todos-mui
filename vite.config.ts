f32ab67943ed49151e93d448cbb10009cc952f90
pbxr250 <pbxr250@yahoo.com>
2024-06-13 00:01:20 +0300

    init2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    rollupOptions: {
+      output: {
+        chunkFileNames: '[name]-[hash].js',
+        entryFileNames: '[name]-[hash].js'
+      }
+    }
   },
   test: {
     globals: true,
